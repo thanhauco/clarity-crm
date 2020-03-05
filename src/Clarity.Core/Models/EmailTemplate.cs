201f93c82efc093f54bb5d122a6dbf90947d56dc
Thanh Vu <thanhauco@gmail.com>
2020-03-05 11:15:00 -0800

    Integrate Email Service for notifications

---
+++ src/Clarity.Core/Models/EmailTemplate.cs
@@ -0,0 +1,1 @@
+namespace Clarity.Core.Models { public class EmailTemplate { public string Id { get; set; } public string Body { get; set; } } }
---
+++ src/Clarity.Services/EmailService.cs
@@ -0,0 +1,8 @@
+using Clarity.Core.Models;
+namespace Clarity.Services {
+    public class EmailService {
+        public void SendWelcomeEmail(string email) {
+            // Send logic
+        }
+    }
+}
